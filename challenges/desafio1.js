dabc9da9a46d4b82a52d32b9db15eea3930ccab9
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-15 17:29:42 -0300

    Melhora na Query, utilização do operador $all

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -4,7 +4,5 @@ db.movies.aggregate([
       { "imdb.rating": { $gte: 7 } },
       { genres: { $nin: ["Crime", "Horror"] } },
       { rated: { $in: ["PG", "G"] } },
-      { languages: "English" },
-      { languages: "Spanish" },
-    ] },
+      { languages: { $all: ["Spanish", "English"] } }] },
   }]);
