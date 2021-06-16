2272541e57be7148894b2eeaf574ecb92267ff39
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-16 02:02:56 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.movies.aggregate([{
+  $addFields: {
+    title_split: { $split: ["$title", " "] },
+  },
+}, {
+  $match: { title_split: { $size: 1 } },
+}, {
+  $project: {
+    _id: 0,
+    title_split: 1,
+  },
+}, {
+  $sort: { title_split: 1 },
+}]);
