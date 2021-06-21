879f14b97773aab748aa1d8d75d201beaf9bc35a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-21 19:02:20 -0300

    Desafio 11 concluido

---
+++ challenges/desafio11.js
@@ -0,0 +1,14 @@
+db.trips.aggregate([
+  { $project: {
+    _id: 0,
+    diaDaSemana: { $dayOfWeek: "$startTime" } } },
+  { $group: {
+    _id: "$diaDaSemana",
+    total: { $sum: 1 } } },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$total" } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
